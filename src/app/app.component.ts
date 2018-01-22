cab8deb7ed49887a676623482d17f13b52b08f23
Hemalatha <hemalatha@olivetech.net>
2018-01-22 17:37:45 +0530

    Routing Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +6,5 @@ import {Hero} from './hero';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  heroes = [
-  
-  new Hero(1,'windStorm'),
-  new Hero(2,'excuse me'),
-  new Hero(3,'myself'),
-  new Hero(4,'yourself')]
-
-  myHero = this.heroes[0];
+  title = 'Tour of Heroes';
 }
